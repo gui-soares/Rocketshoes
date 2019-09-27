143623c739756652343a33cd41e9bc4b7a6029d5
gui-soares <eng.guilhermemsoares@gmail.com>
2019-09-27 10:35:30 -0300

    Configurando Estrutura

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  env: {
+    es6: true,
+  },
+  extends: [
+    'airbnb','prettier', 'prettier/react'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react', 'prettier','react-hooks','jsx-a11y'
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [
+      'warn',
+      { extensions: [ '.jsx', '.js']}
+    ],
+    'import/prefer-default-export': 'off',
+    'no-param-reassign': 'off',
+    'no-console': ['error', { allow: [ 'tron']}],
+    'no-unused-vars': [ 'error', { argsIgnorePattern: '^_'}],
+    'global-require': 'off',
+    'no-urderscore-dangle': 'off',
+    camelcase: 'off',
+    'react/jsx-one-expression-per-line': 'off',
+    'react-native/no-raw-text': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
+  },
+};
