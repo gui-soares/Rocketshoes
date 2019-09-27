17818434a78f8bf1d03300b33d9b1cbb2b70c685
gui-soares <eng.guilhermemsoares@gmail.com>
2019-09-27 10:54:03 -0300

    Configurando Rotas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import {BrowserRouter} from 'react-router-dom';
+
+import Routes from './routes';
 
 function App() {
   return (
-    <h1>Title</h1>
-  );
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  )
 }
 
 export default App;
